6998aa477aa45d399b52fb9a6f5b8e065250f29a
edwin <arayamaicol201277@gmail.com>
2026-05-15 07:30:00 -0600

    avance

--- Proyecto 1/Pandemic/Pandemic.c
+++ Proyecto 1/Pandemic/Pandemic.c
@@ -13,6 +13,13 @@ struct people{
     struct people* sigt;
     struct people* ante;
 };
+
+struct pleplo{
+    char nombre[20];
+    int edad;
+    struct people* sigt;
+    struct people* ante;
+};
 //Dnodo es por el doblemente enlazada, para diferenciar de la no enlazada y de los nodos de árboles, al igual que Dlista
 struct Dnodo{
     char pais[25]; //Se puede hacer mejor xd
